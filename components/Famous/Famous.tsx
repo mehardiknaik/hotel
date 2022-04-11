17520262ff58ccd301b7f97916788f2f2bcf1c1d
Hardik Naik <hardiknaik7444@gmail.com>
2022-04-11 15:36:00 +0530

    initial commit

---
+++ components/Famous/Famous.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Famous = () => {
+  return (
+    <div>Famous</div>
+  )
+}
+
+export default Famous
---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Famous from '../components/Famous/Famous'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <div>
+      Initial commit
+    </div>
+    <Famous/>
+    </>
+  )
+}
+
+export default Home
