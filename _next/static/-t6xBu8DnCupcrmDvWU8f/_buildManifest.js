74e5038b863ac12e2b50b384c0a1d38e47103de1
Hardik Naik <hardiknaik7444@gmail.com>
2022-04-12 10:14:22 +0530

    Updates

---
+++ _next/static/-t6xBu8DnCupcrmDvWU8f/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/125-78fc8878a41c3d7f.js","static/chunks/pages/index-b1f73cbfc319ed41.js"],"/_error":["static/chunks/pages/_error-427958541d1e32b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
