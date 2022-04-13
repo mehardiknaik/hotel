410b4d128e4c22c8bb5909ef7612710363fa2a5e
Hardik Naik <hardiknaik7444@gmail.com>
2022-04-13 12:17:55 +0530

    global changes

--- styles/global.ts
+++ styles/global.ts
@@ -1,14 +1,35 @@
 import { createGlobalStyle } from "styled-components";
 
+const light = `{
+    --backgroundprimary:#dcdcdc;
+    --backgroundsecondary:#f8c7ff;
+    --textprimary:#353535;
+
+}`
+
+const dark = `{
+    --backgroundprimary:#353535;
+    --backgroundsecondary:#300037;
+    --textprimary:#dcdcdc;
+
+}`
 
 export const GlobalStyle = createGlobalStyle`
-:root{
-    --backgroundprimary:${({ theme }) => theme == 'light' ? "#dcdcdc" : "#353535"};
-    --backgroundsecondary:${({ theme }) => theme == 'light' ? "#f8c7ff" : "#300037"};
-    --textprimary:${({ theme }) => theme == 'light' ?  "#353535":"#dcdcdc"};
-}
+:root${({ theme }) => theme == 'light' ? light : dark}
+html,
 body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     background:var(--backgroundprimary)
 }
+a {
+  color: inherit;
+  text-decoration: none;
+}
 
+* {
+  box-sizing: border-box;
+}
 `
