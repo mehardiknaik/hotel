8622a0b664223c99dde804cc102b53c4d3308815
Hardik Naik <hardiknaik7444@gmail.com>
2022-04-14 20:34:06 +0530

    Updates

---
+++ _next/static/ZgAUTyYd_gUYKcebAcnY3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/953-8545dd543fe172e8.js","static/chunks/pages/index-50e29fbe712ae9f2.js"],"/_error":["static/chunks/pages/_error-427958541d1e32b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/yWhWuOKOELS1xXj5FzCGu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/953-8545dd543fe172e8.js","static/chunks/pages/index-cbf6d1c758765c5f.js"],"/_error":["static/chunks/pages/_error-427958541d1e32b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
