0a6cddea2bc00363a2e3dd1fba4c6e1d7df549c9
Hardik Naik <hardiknaik7444@gmail.com>
2022-07-14 16:43:49 +0530

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,13 +2,10 @@
 import fs  from 'fs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
